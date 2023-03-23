6ec87db35f3f9f2c7e7e95057c950d85e806fb3f
pier <piergiuseppe.mallozzi@gmail.com>
2023-03-22 18:34:52 -0700

    comment change

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from trusted_party.sign_data import sign_data
 from utils.generate_data import generate_genomic_data
 from shared.paths import data_file_path, signature_data_file_path
 
-# Generate random data
+# Generate random genomic data
 generate_genomic_data(5, data_file_path)
 # # Trusted party signs data authenticity
 sign_data(data_file_path, signature_data_file_path)
