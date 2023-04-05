7a81d5a5788f9e98b32b8ef72ff1f1621f0c40a9
“Michele” <bonini.michele@yahoo.it>
2023-04-05 23:12:57 +0200

    read/write map features added

--- smart_contracts/AlgorandSmartContract/playground/hello_world/test.py
+++ smart_contracts/AlgorandSmartContract/playground/hello_world/test.py
@@ -1,14 +1,23 @@
 import beaker as bk
 import pyteal as pt
-
+from beaker.lib.storage import BoxList, BoxMapping
 
 class MyState:
     result = bk.GlobalStateValue(pt.TealType.uint64)
 
+    # publicKeys = bk.GlobalStateValue(key=pt.abi.uint64, stack_type=pt.abi.String)
+    publicKeys = bk.ReservedGlobalStateValue(
+        stack_type=pt.abi.String,
+        max_keys=32,
+        descr="A reserved app state variable, with 32 possible keys",
+    )
+
+    authorizedUsers = bk.GlobalStateValue(pt.abi.Array)
+
 app = bk.Application("test", state=MyState())
 
-@app.external
-def add(a: pt.abi.Uint64, b: pt.abit.Uint64, *, output: pt.abi.Uint64) -> pt.Expr:
+@app.external#(authorize=beaker.Authorize.only_creator())
+def add(a: pt.abi.Uint64, b: pt.abi.Uint64, *, output: pt.abi.Uint64) -> pt.Expr:
     add_result = a.get() + b.get()
     return pt.Seq(
         app.state.result.set(add_result),
@@ -19,8 +28,16 @@ def add(a: pt.abi.Uint64, b: pt.abit.Uint64, *, output: pt.abi.Uint64) -> pt.Exp
 def read_result(*, output: pt.abi.Uint64) -> pt.Expr:
     return output.set(app.state.result)
 
+
+
 @app.external
-def getPublicKey(userAddress:  pt.abi.)
+def set_reserved_global_state_publicKeys(tokenId: pt.abi.Uint8, publicKeyHospital: pt.abi.String) -> pt.Expr:
+    return app.state.reserved_global_value[tokenId].set(publicKeyHospital.get())
+
+
+@app.external(read_only=True)
+def get_reserved_global_state_publicKeys(tokenId: pt.abi.Uint8, *, publicKeyHospital: pt.abi.String) -> pt.Expr:
+    return publicKeyHospital.set(app.state.reserved_global_value[tokenId])
 
 
 
@@ -29,13 +46,3 @@ def getPublicKey(userAddress:  pt.abi.)
 #     spec.export("artifacts")
 
 
-# function getPublicKey(address userAddress, string memory name)
-
-
-# function setPublicKey(string memory name, string memory publicKey) 
-
-# function authorizeUser(address userAddress) publ
-
-#  function revokeUser(address userAddress) public {
-
-#     function isUserAuthorized(address userAddress
