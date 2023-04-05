595f0d8d62a37506dc422e8010bd146ef8c8ac22
Guillaume Lethuillier <66550865+glethuillier@users.noreply.github.com>
2023-04-05 09:17:50 +0200

    zkp token: small fix

--- smart_contracts/ethereum/ZkpContract/contracts/zkpToken.sol
+++ smart_contracts/ethereum/ZkpContract/contracts/zkpToken.sol
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT
+/// @author Guillaume Lethuillier
+
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
@@ -7,8 +10,7 @@ contract ZkpToken is ERC721 {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() ERC721("ZKP Token", "ZKP") public {
-    }
+    constructor() ERC721("ZKP Token", "ZKP") {}
 
     function mint(address user) public returns (uint256) {
         _tokenIds.increment();
