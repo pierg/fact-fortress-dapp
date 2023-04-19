5f67546699875095bae0d9b97b8f6bccfafe3c21
pier <piergiuseppe.mallozzi@gmail.com>
2023-04-18 20:09:16 -0700

    cleanup

--- tools/utils/src/shared/__init__.py
+++
@@ -1,4 +0,0 @@
-from .paths import *
-
-
-sum_size = 8
--- tools/utils/src/shared/paths.py
+++
@@ -1,5 +0,0 @@
-import pathlib
-
-data_folder_path = pathlib.Path(__file__).parent.parent.parent / "data"
-
-circuits_path = pathlib.Path(__file__).parent.parent / "circuits"
